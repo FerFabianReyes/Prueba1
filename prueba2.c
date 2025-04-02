cea6bff99f33b35f56977b161abf55a9a398389c
ferfabianreyes <2100721a@umich.mx>
2025-04-01 18:21:46 -0600

    cambie el main de la prueba2

--- prueba2.c
+++ prueba2.c
@@ -1,15 +1,12 @@
 /* ejemplo 4.- se realizan conversiones de tipos implícitas y
 explícitas */
 #include <stdio.h>
-int main() {
-double d , e , f = 2.33 ;
-int i = 6 ;
-e = f * i ; /* e es un double de valor 13.98*/
-printf( "Resultado = %f\n", e);
-d = (int) ( f * i ) ; /* d es un double de valor 13.00*/
-printf( "Resultado = %f\n", d);
-d = (int) f * i ; /* f se ha convertido a un entero truncando*/
-/*sus decimales, d es un double de valor 13.00*/
-printf( "Resultado = %f\n", d);
-return 0;
-}
+int main () {
+    printf(" %f \n", va_float); /* imprime 33.241*/
+    printf(" %.1f \n", va_float); /* imprime 33.2*/
+    printf(" %.4f \n", va_float); /* imprime 33.2410*/
+    printf(" %1.4e \n", va_float); /* imprime 3.3241e+01*/
+    printf(" %d \n", va_int); /* imprime 805*/
+    printf(" %10f\n", va_float); /* imprime 33.241*/
+    return 0;
+    }
