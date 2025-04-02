3e1498c577ddf3db13bd73ba43b55db8689e3e5e
ferfabianreyes <2100721a@umich.mx>
2025-04-01 18:18:10 -0600

    puse código en los dos archivos

---
+++ prueba1.c
@@ -0,0 +1,14 @@
+/* ejemplo 2.- multiplica dos números enteros y muestra el
+resultado por pantalla */
+#include <stdio.h>
+int main( )
+{
+int multiplicador; /*se define multiplicador como un entero */
+int multiplicando; /*se define multiplicando como un entero */
+int res; /*se define resultado como un entero*/
+multiplicador = 1000; /*se asignan valores*/
+multiplicando=2;
+res=multiplicador*multiplicando;
+printf("Resultado =%d\n",res); /*se muestra el resultado */
+return 0;
+}
