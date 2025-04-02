a11bd5e0c0a8710692a4d0ac57fc70b2c970f04a
ferfabianreyes <2100721a@umich.mx>
2025-04-01 18:23:20 -0600

    Se arrgegló el error

--- prueba2.c
+++ prueba2.c
@@ -2,11 +2,11 @@
 explícitas */
 #include <stdio.h>
 int main () {
-    printf(" %f \n", va_float); /* imprime 33.241*/
-    printf(" %.1f \n", va_float); /* imprime 33.2*/
-    printf(" %.4f \n", va_float); /* imprime 33.2410*/
-    printf(" %1.4e \n", va_float); /* imprime 3.3241e+01*/
-    printf(" %d \n", va_int); /* imprime 805*/
-    printf(" %10f\n", va_float); /* imprime 33.241*/
+    printf(" %f \n", 1); /* imprime 33.241*/
+    printf(" %.1f \n", 2); /* imprime 33.2*/
+    printf(" %.4f \n", 3); /* imprime 33.2410*/
+    printf(" %1.4e \n", 4); /* imprime 3.3241e+01*/
+    printf(" %d \n", 5); /* imprime 805*/
+    printf(" %10f\n", 5); /* imprime 33.241*/
     return 0;
     }
