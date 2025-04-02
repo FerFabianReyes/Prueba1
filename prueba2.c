9822790d802ac481f17f68addd22d691fec47e9c
ferfabianreyes <2100721a@umich.mx>
2025-04-01 18:33:28 -0600

    cambios en un comentario

--- prueba2.c
+++ prueba2.c
@@ -4,7 +4,7 @@ explícitas */
 int main () {
     printf(" %f \n", 1); /* imprime 33.241*/
     printf(" %.1f \n", 2); /* imprime 33.2*/
-    printf(" %.4f \n", 3); /* imprime 33.2410*/
+    printf(" %.4f \n", 3); /* imprime 33.2410 otro cambio al arciasdfjjaflkasklfjl*/ 
     printf(" %1.4e \n", 4); /* imprime 3.3241e+01*/
     printf(" %d \n", 5); /* imprime 805*/
     printf(" %10f\n", 5); /* imprime 33.241*/
